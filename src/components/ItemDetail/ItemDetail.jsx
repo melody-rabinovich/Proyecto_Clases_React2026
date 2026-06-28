e69dc6a9d55b3a5b4dac11bfea6b31e76bb89acb
Admin <admin@MacBook-Pro.local>
2026-06-27 21:55:04 -0300

    clase 8 - contexto

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,14 @@
 import "./ItemDetail.css"
 import {Item} from"../Item/Item.jsx"
+import {useCart} from "../../context/CartContext.jsx"
+
 export const ItemDetail = ({item}) => {
 
-    return(
+    const {addItem} = useCart()
+
+    return( 
         <Item {...item}>
-            <button className="btn-primary">
+            <button className="btn-primary" onClick={() => addItem(item)}>
                 Argegar al carrito
             </button>
         </Item>
